c87248b8f2735945fa1df06e8342346d0242ffe9
Pobarul-islam <pobarul3@gmail.com>
2022-09-25 11:44:24 +0600

    email error added

--- pages/Login.js
+++ pages/Login.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  function submitHandler() {
+    if (!email || !password) {
+      setError('Please Enter Your Email and Password')
+      return;
+    }
+  }
+
   return (
     <div className="flex-1 text-xs sm:text-sm flex flex-col justify-center items-center gap-2 sm:gap-4">
       <h1 className="font-extrabold select-none text-2xl sm:text-4xl uppercase">
         Login
       </h1>
+      {error && (
+        <div className="w-full max-w-[40ch] border-rose-400 border text-center border-solid text-rose-400 py-2">
+          {error}
+        </div>
+      )}
       <input
         type="text"
-       
         onChange={(e) => setEmail(e.target.value)}
+        value={email}
         placeholder="Email Address"
         className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-300 text-slate-900 p-2 w-full max-w-[40ch]"
       />
       <input
-       
         onChange={(e) => setPassword(e.target.value)}
+        value={password}
         type="password"
         placeholder="Password"
         className="outline-none text-slate-900 p-2 w-full max-w-[40ch] duration-300 border-b-2 border-solid border-white focus:border-cyan-300"
       />
       <button
-        
+        onClick={submitHandler}
         className="w-full max-w-[40ch] border border-white border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900"
       >
         <h2 className="relative z-20">SUBMIT</h2>
